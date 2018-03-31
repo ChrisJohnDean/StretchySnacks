0bae3b07737a4637e6dc8db41427fda4fdba8bb4
Chris Dean <chrisjgdean@gmail.com>
2018-03-30 18:27:59 -0700

    added tap gestures to imageviews  with action that addes the selected images name to the tableview

--- StretchySnacks/StretchyViewController.swift
+++ StretchySnacks/StretchyViewController.swift
@@ -9,13 +9,16 @@
 import UIKit
 
 
-class StretchyViewController: UIViewController {
+class StretchyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var heightConstraint: NSLayoutConstraint!
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var plusIconOutlet: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     
+//    var selectedImage
     var imageViewArray = Array<UIImageView>()
+    var selectedImages = Array<String>()
     var plusButtonSelected: Bool = false
     var stackView: UIStackView!
     
@@ -32,8 +35,11 @@ class StretchyViewController: UIViewController {
         imageViewArray.append(popsicleView)
         let ramenView = UIImageView(image: #imageLiteral(resourceName: "ramen"))
         imageViewArray.append(ramenView)
+        addTapGesture()
         createStackView()
         stackView.isHidden = true
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,8 +53,36 @@ class StretchyViewController: UIViewController {
     }
 
     @IBAction func plusIconAction(_ sender: Any) {
-
         animateSpringyHeader()
+    }
+    
+    func addTapGesture() {
+        for i in imageViewArray {
+            i.tag = imageViewArray.index(of: i)!
+            i.isUserInteractionEnabled = true
+            let tap = UITapGestureRecognizer(target: self, action: #selector(StretchyViewController.didTapImage))
+            i.addGestureRecognizer(tap)
+        }
+    }
+    
+    @objc private func didTapImage(sender: UITapGestureRecognizer) {
+        
+        guard let tag = sender.view?.tag else {return}
+        switch tag {
+        case 0:
+            selectedImages.append("Oreos")
+        case 1:
+            selectedImages.append("Pizza Pockets")
+        case 2:
+            selectedImages.append("Poptarts")
+        case 3:
+            selectedImages.append("Popsicle")
+        case 4:
+            selectedImages.append("Ramen")
+        default:
+            break
+        }
+        tableView.reloadData()
     }
     
     func createStackView() {
@@ -84,14 +118,27 @@ class StretchyViewController: UIViewController {
             plusIconOutlet.transform = rotate
             stackView.isHidden = false
             UIView.animate(withDuration: 2, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.2, options: .curveEaseInOut, animations: {
-                
                 self.heightConstraint.constant = 200
                 self.view.layoutIfNeeded()
                 self.plusButtonSelected = true
             }, completion: nil)
-            
-            
         }
+    }
+    
+}
+
+// UITableViewDataSource methods
+extension StretchyViewController {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return selectedImages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let selectedString = selectedImages[indexPath.row]
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = selectedString
+        return cell
     }
     
 }
